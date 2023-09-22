966f0e6ada2de79638ae3b2141301db0718fecf8
Anthony <thewarparmoury@gmail.com>
2023-09-22 00:21:41 -0500

    added BattleAxe class

---
+++ Models/BattleAxe.cs
@@ -0,0 +1,21 @@
+﻿using Class05.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class05.Models
+{
+    public class BattleAxe : IWeapon
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+
+        public BattleAxe(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+    }
+}
