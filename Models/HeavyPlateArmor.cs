788b0d2baad9d0baa626b0e88a0bc6776e65bb68
Anthony <thewarparmoury@gmail.com>
2023-09-22 00:35:02 -0500

    added HeavyPlateArmor Class. Updated Main to call new fighter, armor, weapon

---
+++ Models/HeavyPlateArmor.cs
@@ -0,0 +1,22 @@
+﻿using Class05.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class05.Models
+{
+    public class HeavyPlateArmor : IArmor
+    {
+        public string Name { get; set; }
+        public int Defense { get; set; }
+
+        // constructor
+        public HeavyPlateArmor(string name, int defense)
+        {
+            Name = name;
+            Defense = defense;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,12 +9,17 @@ namespace Class05
         {
 
             IArmor armor = new Leather("Elven Leather Armor", 12);
+            IArmor heavyPlateArmor = new HeavyPlateArmor("Dwarven Plate Armor", 25);
             IWeapon sword = new Sword("Long sword", 5);
+            IWeapon battleaxe = new BattleAxe("Executioner's BattleAxe", 18);
+
 
             ICombatant fighter = new Fighter("Fighter Fred", armor, sword);
             ICombatant badGuy = new Fighter("Scary Monster", armor, sword);
+            ICombatant dwarvenFighter = new Fighter("Gimli", heavyPlateArmor,battleaxe);
 
             fighter.Attack(badGuy);
+            dwarvenFighter.Attack(badGuy);
 
         }
 
